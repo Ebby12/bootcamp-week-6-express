a1b9a166919477a3aef3ff2d8edd587e4e6ced48
Kat Law <kathleen.law@multiverse.io>
2023-05-10 13:34:03 -0400

    router and tests (#3)

---
+++ __tests__/app.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../src/app.js');
+const User = require('../src/models/User');
+
+jest.mock('../src/models/User.js', () => ({ create: jest.fn() }))
+describe("User routes", () => {
+  // this is the structure you should strive for!
+  // it('should retrieve all users', async () => {
+  //   const response = await request(app).get('/users');
+  //   expect(response.statusCode).toBe(200);
+  //   const parsedResponse = JSON.parse(response.text);
+    
+  //   expect(parsedResponse[0].username).toBe('kat1')
+  // })
+
+  // the next test below is extra
+  it('should create a user', async () => {
+    User.create.mockResolvedValue({'username': 'testKat', 'email': 'test@test.com', 'password': '2222'});
+    
+    const response = await request(app).post('/users').send({'username': 'testKat', 'email': 'test@test.com', 'password': '2222'})
+
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe('testKat');
+    expect(User.create).toHaveBeenCalledWith({'username': 'testKat', 'email': 'test@test.com', 'password': '2222'})
+  })
+})
--- src/app.js
+++ src/app.js
@@ -1,61 +1,9 @@
 const express = require('express');
 const app = express();
-const User = require('./models/User.js');
+const usersRouter = require('./routes/users.js');
 
 app.use(express.json());
 
-app.get('/users', async (request, response) => {
-  // response.send(`This is a ${request.method} method`)
-  const users = await User.findAll();
-  response.send(users);
-});
-
-app.post('/users', async (req, res, next) => {
-  try {
-    const user = await User.create(req.body)
-    res.send(user.username);
-  } catch (error) {
-    next(error);
-  }
-})
-
-app.get('/users/:username', async (req, res, next) => {
-  try {
-    const user = await User.findOne({
-      where: {username: req.params.username}
-    })
-    res.send(user);
-  } catch (error) {
-    next(error);
-  }
-})
-
-// PUT request that uses req.params.username to find a user, and req.body to update the user with whatever data is in there. sendStatus returns a successful Status Code of 200 if updated successfully
-app.put("/users/:username", async (req, res, next) => {
-  try {
-    const updated = await User.update(req.body, {
-      where: { username: req.params.username },
-    });
-    console.log(updated);
-    res.sendStatus(200);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// DELETE request that uses req.params.username to find a user and delete them. sendStatus returns a successful Status Code of 200 if deleted successfully
-app.delete("/:username", async (req, res, next) => {
-  try {
-    const deleted = await User.destroy({
-      where: { username: req.params.username },
-    });
-    if (deleted === 0) {
-      throw new Error("No user deleted");
-    }
-    res.sendStatus(200);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use('/users', usersRouter);
 
 module.exports = app;
